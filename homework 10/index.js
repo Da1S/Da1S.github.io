2554924dca75dbc61385ad0aac8429c95789c024
Da1S <62472586+Da1S@users.noreply.github.com>
2020-06-04 15:09:47 +0300

    Add files via upload

---
+++ homework 10/index.js
@@ -0,0 +1,10 @@
+alert("Hello, world!!");
+// console.log("Hello world");
+
+let a = "hello world";
+let b = "what are you doing";
+let c = "freely done?";
+
+console.log(a);
+console.log(b);
+console.log(c);
