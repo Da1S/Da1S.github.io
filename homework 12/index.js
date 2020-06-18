a9e77955284000e182daa806b1941b6b03be5060
Da1S <62472586+Da1S@users.noreply.github.com>
2020-06-18 13:16:34 +0300

    Add files via upload

---
+++ homework 12/index.js
@@ -0,0 +1,23 @@
+AOS.init({
+    offset:400,
+    delay:0,
+    duration:1000
+});
+
+
+let message = prompt("правильно зробив домашнє?");
+ 
+let massage1 = prompt("скіки балів ");
+
+if (massage1 == 3 ){
+    alert ("дякую я старався ");
+}
+else{
+    alert ("  плак плак ");
+}
+
+if(message == та){
+    alert(" :)")
+}else{
+    alert (" :( ) ")
+};
